8ca7a49da0db7a8535222e1bddf1de61a1a32e57
MS MASKUR CHOWDHURY <chymaskur@gmail.com>
2021-01-22 19:26:10 +0300

    first commit

---
+++ assignment3.js
@@ -0,0 +1,51 @@
+//github/link
+
+
+kilometerToMeter
+function kilometerToMeter(meter){
+    const kilometer = meter * 1000;
+    return kilometer;
+}
+
+var result = kilometerToMeter(2);
+console.log(result);
+
+
+
+
+//budgetCalculator
+
+
+
+//hotelCost
+
+
+//megaFriend
+
+
+
+
+function megaFriend(friendName){
+
+    var megaFriend = friendName[0];
+ 
+    for(var i= 0 ; i< megaFriend.length; i++){
+    var element = friendName[i];
+    if(element < megaFriend){
+    megaFriend = element;
+    }
+    }
+    return megaFriend;
+     
+}
+    var output=megaFriend(["Jabbar", "Barakat", "Rafiq", "AlFazalTiziahrah", "WindowsKhan", "hossian"]);
+    console.log(output);
+
+
+
+
+
+
+
+
+
